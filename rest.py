9382c58301528de4baa1320f062506a90c176092
Anderson Sacramento <anderson.sacramento93@gmail.com>
2015-09-28 01:30:28 -0300

    Versão com implementação total do webservice, falta testa os métodos de inserção e solicitação dos resultados. Quanto ao cliente é necessário implementar o processo de solicitação dos resultados e apresentação dos gráficos utilizando highcharts.

--- rest.py
+++ rest.py
@@ -82,69 +82,148 @@ class device_config:
 
 class task_result:
     #Obtem resultados browser -> webservice
-    def GET(self,id, device_name):
-        result = tasks[codigo]
+    def GET(self):
+        data = web.input()
+        device_name = data.device_name
+        id_task = data.id_task
+        client_name = data.client_name
+        result = get_task_result(client_name, device_name, codigo_task)
         return result
     
     #Fornece dados device -> webservice    
-    def POST(self,codigo):
+    def POST(self):
         data = web.input()
-        ip = data.device_ip
-        sensor_data = data.sensor_data
-        
+        device_name = data.device_name
+        id_task = data.id_task
+        client_name = data.client_name
+        result = data.result
+        save_result(client_name,device_name,id_task,result)
+
+    def get_task_result(client_name, device_name, codigo_task):
+        client_name = str(client_name)
+        device_name = str(device_name)
+        codigo_task = int(codigo_task)
+
+        s = shelve.open('shelve_devices_agenda.db', flag = 'c',writeback = True)
+        try:
+            if not s.has_key(device_name):
+                s.close()
+                return False
+            if not s[device_name]:
+                s.close()
+                return False
+            if s[device_name][client_name][id_task]['done']:
+                s.close()
+                return s[device_name][client_name][id_task]['result']
+            else:
+                s.close()
+                return None
+        finally:
+            s.close()
+
+
+    def save_result(self, client_name, device_name, id_task, result):
+        client_name = str(client_name)
+        device_name = str(device_name)
+        id_task = int(id_task)
+        s = shelve.open('shelve_devices_agenda.db', flag = 'c',writeback = True)
+        try:
+            if not s.has_key(device_name):
+                s.close()
+                return False
+            if not s[device_name]:
+                s.close()
+                return False
+            s[device_name][client_name][id_task]['result'] = result
+            s[device_name][client_name][id_task]['done'] = True
+        finally:
+            s.close()
+
 class task_agenda_device:
     def GET(self, device_name):
         task  = self.get_first_undone_task(device_name) #exemplo de dados de uma tarefa
-        if not task: return None
+        if not task: return []
         io  = StringIO()
         json.dump(task,io)
         output = io.getvalue()
         return output
 
     def get_first_undone_task(self, device_name):
+        device_name = str(device_name)
         s = shelve.open('shelve_devices_agenda.db', flag = 'c')
         try:
-            device = str(device_name)
-            if not s.has_key(device): raise KeyError
-            agenda = s[device]['agenda'] 
-            tasks =  agenda.keys()
-            for t in tasks:
-                if t['done'] == False:
-                    id_last_undone = t.key()
-                    break
-            else:
-                s.close()
-                return {'id':id_last_undone, 'duration':t['duration'], 'frequency':t[frequency],'done':t['done']}
+            if not s.has_key(device_name): raise KeyError
+            agenda_clients = s[device_name].values()
+            for client in agenda_clients:
+                for task in client:
+                    if task['done'] == False:
+                        s.close()
+                        task['device_name'] = device_name
+                        return task 
         except KeyError:
             print 'Do not has key.'
         finally:
             print 'close shelve'
             s.close()
         return None
-
-class task_agenda:        
+    
+class task_agenda:
+    def GET(self):
+        web.header('Content-Type', 'application/json')
+        web.header('Access-Control-Allow-Origin', '*')
+        data = web.input()
+        client_name = data.client_name
+        device_name = data.device_name
+        return self.get_tasks_in_agenda(client_name, device_name)
+    
     def POST(self):
         data = web.input() # you can get data use this method
+        client_name = data.client_name
         device_name =  data.device_name
         duration = data.duration_seconds
         frequency = data.frequency_milliseconds
-        self.save_agenda(device_name, duration, frequency)
+        self.save_agenda(client_name,device_name, duration, frequency)
         #dev = DeviceConnection(ip,port,duration)
         #dev.startConnection()
 
-    def save_agenda(self, device_name, duration, frequency):
+    def get_tasks_in_agenda(self,client_name, device_name):
+        client_name = str(client_name)
+        device_name = str(device_name)
+        print(client_name,device_name)
         s = shelve.open('shelve_devices_agenda.db', flag = 'c',writeback = True)
         try:
-            device = str(device_name)
-            if not s.has_key(device):
-                id = 1
-            else:
-                last = s[device]['agenda'].last()
-                id = int(last.key())
-            print 'New id:',id
-            s[device]['agenda'][id]['duration'] = duration
-            s[device]['agenda'][id]['frequency'] = frequency
-            s[device]['agenda'][id]['done'] = False
+            if not s.has_key(device_name):
+                s.close()
+                return []
+            ids =  []
+            for task in s[device_name][client_name]:
+                ids.append(task['id'])
+                print(task, task['id'])
+            print(ids)
+        finally:
+            s.close()
+        return ids
+
+    def save_agenda(self, client_name, device_name, duration, frequency):
+        client_name = str(client_name)
+        device_name = str(device_name)
+        duration = int(duration)
+        frequency = int(frequency)
+        s = shelve.open('shelve_devices_agenda.db', flag = 'c',writeback = True)
+        try:
+            if not s.has_key(device_name):
+                s[device_name] = {}
+
+            if not s[device_name].has_key(client_name):
+                s[device_name][client_name] = []
+            id_task = len(s[device_name][client_name])
+            print 'New id:',id_task
+            s[device_name][client_name].append({
+                'id':id_task,
+                'duration':duration,
+                'frequency':frequency,
+                'done':False
+            })
         finally:
             s.close()
 
